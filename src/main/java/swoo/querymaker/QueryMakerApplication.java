edfbfefca307f5e8d447424630fc2bd5c74e0304
sangwoo <lesw1216@gmail.com>
2023-12-30 19:59:14 +0900

    init: init project

---
+++ src/main/java/swoo/querymaker/QueryMakerApplication.java
@@ -0,0 +1,13 @@
+package swoo.querymaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueryMakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueryMakerApplication.class, args);
+	}
+
+}
