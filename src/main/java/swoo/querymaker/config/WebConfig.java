ae16e7b01385a2ce8d8d65acbb70e3dccc7fb06c
sangwoo <lesw1216@gmail.com>
2024-02-08 19:57:24 +0900

    refactor: main 배포를 위한 개발용 CORS 설정 해제

--- src/main/java/swoo/querymaker/config/WebConfig.java
+++ src/main/java/swoo/querymaker/config/WebConfig.java
@@ -10,9 +10,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
-                .allowCredentials(true)
-                .allowedMethods("GET", "POST", "PUT", "DELETE");
+//        registry.addMapping("/**")
+//                .allowedOrigins("http://localhost:5173")
+//                .allowCredentials(true)
+//                .allowedMethods("GET", "POST", "PUT", "DELETE");
     }
 }
